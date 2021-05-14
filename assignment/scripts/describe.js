f1c97205bb67ea3da79ff085f36432a2a801ea5d
Duy Dao <jimmydao@gmail.com>
2021-05-14 09:39:26 -0500

    pretty much done with describe.js but have questions

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,6 +1,6 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
@@ -26,9 +26,9 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// Line 17 lets us know that the variable's name is called "number" and the value of number is "1".
+// Line 19 is an increment operator and it would not increase the value compared to if it was typed out ++number instead.
+// Line 21 is a Conditional which states that if the variable is greater than or equal to 2 that the console.log will display yes in the console.
 //
 
 //CODE
@@ -45,9 +45,9 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// Line 36 states that the variable's name is "name" and the value is a string called "Dane".
+// For the condition on Line 38 the console that would populate would be How do you do? The reason for
+// this is this is because the conditions were not met. The variable "name" does not match.
 //
 
 //CODE
@@ -70,10 +70,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// In Line 55 there is no value stated for the variable "secret" but it is later assigned a string
+// value of "super" in Line 60.
+// In Line 57 the variable is named "code" and given the value of 123.
+// In Line 59 the condition states that the variable code has to be true and an exact match,
+// which in this case it is. It is also followed by setting a string value super to the variable
+// secret. In line 61 the variable code is given a new value which is 123 multiplied by 2 which is 246.
+// The second condition in line 64 states that code is less than 250 and the variable secret is given
+// a new string value of duper. Since the condition was not met in Line 64 the console will not populate.
 
 //CODE
 /*
@@ -97,7 +101,7 @@ if (isStudent === true && zip > 80000 ) {
 
 
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -106,16 +110,16 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number-- decrements number, but the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -125,9 +129,14 @@ if (number >= 2) {
 
 
 
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
+
+  //The variable colorOne and colorTwo does not match the description. To correct this you would need to change
+  //colorOne's color to blue and colorTwo's color go red. In the condition the variable mix does equal
+  //true so this would change colorOne to purple but colorTwo variable is missing in this condition. You
+  //would need to add the code +colorTwo = 'purple' between the colorOne variable and the semicolon.
 
 /*
 let colorOne = 'red';
@@ -140,9 +149,13 @@ if (mix === true) {
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
+  //Everything is correct minus the compound conditional of "||" which would mean either side
+  //or variable can be true to satisfy the condition. It would need to instead be changed to "&&"
+  //instead to require both variables to meet the condition per requested in the description.
+  //**QUESTION HERE: Does there need to be a "let" before "const time = 4"???
 /*
 let temp = 40;
 const time = 4;
@@ -153,8 +166,14 @@ if (temp > 39 || time >= 4) {
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
+
+  //The request is to pit the age variable to the minAge varible but the condition below
+  //is typed out the wrong way. It should look "if(age >= minAge)". The if and else console.log
+  //should be switched around. If the condition is met the console.log of enter should appear
+  //while the else console.log of no entry should come up if the condition is not met which in
+  //this case the condition is met. The console.log enter would appear to match the request. 
 
 /*
 let age = 21;
@@ -166,4 +185,3 @@ if(minAge <= age) {
   console.log('enter');
 }
 */
-
